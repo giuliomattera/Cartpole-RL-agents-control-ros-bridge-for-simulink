a43c94ea4df1d391166c93d67fe99f495d9e32ae
Giulio Mattera <97847032+giuliomattera@users.noreply.github.com>
2022-03-04 10:47:21 +0100

    Add files via upload

---
+++ simulink/matlab_node.m
@@ -0,0 +1,14 @@
+Episode_time = 10
+rosinit()
+while true
+    start = rossubscriber('start_simulation');
+    ini = receive(start);
+    ini = ini.Data;
+    if (ini == 1)
+      disp('Starting simulation...')
+      sim('simulink_nodes',Episode_time)
+     else
+        disp('No starting recieved from python')
+    end
+end
+
