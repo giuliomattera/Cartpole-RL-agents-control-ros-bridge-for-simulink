a1226e2d20a5da2a0752fb969adfaaaf56a64c70
Giulio Mattera <97847032+giuliomattera@users.noreply.github.com>
2022-03-18 11:53:53 +0100

    Update launch.bash

--- launch.bash
+++ launch.bash
@@ -1,7 +1,5 @@
 roscore &
-gnome-terminal -- cd rl_prj &
-rosrun rl_connection agent_node.py &
-gnome-terminal -- --logdir ./gradient_tape &
+gnome-terminal -- rosrun rl_connection agent_node.py &
 gnome-terminal -- matlab -nodesktop -r "run('./simulink/matlab_node.m')" &
 
 
