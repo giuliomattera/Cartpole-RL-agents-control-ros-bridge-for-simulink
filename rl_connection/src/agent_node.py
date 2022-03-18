6568d862d6024d9afc1d15958d91a843c03b9c12
Giulio Mattera <97847032+giuliomattera@users.noreply.github.com>
2022-03-18 11:39:03 +0100

    Add files via upload

--- rl_connection/src/agent_node.py
+++ rl_connection/src/agent_node.py
@@ -24,9 +24,19 @@ class DDPG():
 
         input = tf.keras.layers.Input(shape=(self.num_states,))
 
-        hidden = tf.keras.layers.Dense(128, activation="relu", kernel_initializer= initializer)(input)
+        hidden = tf.keras.layers.Dense(200, 
+        activation=tf.keras.layers.LeakyReLU(alpha=0.01), 
+        kernel_initializer= initializer,
+        kernel_regularizer=tf.keras.regularizers.l1_l2(l1=1e-5, l2=1e-4)
+        )(input)
 
-        hidden2 = tf.keras.layers.Dense(200, activation="relu",  kernel_initializer= initializer)(hidden)
+        hidden2 = tf.keras.layers.Dense(128, activation=tf.keras.layers.LeakyReLU(alpha=0.1),
+          kernel_initializer= initializer,
+          kernel_regularizer=tf.keras.regularizers.l1_l2(l1=1e-5, l2=1e-4))(hidden)
+
+        hidden3 = tf.keras.layers.Dense(64, activation=tf.keras.layers.LeakyReLU(alpha=0.1),
+          kernel_initializer= initializer,
+          kernel_regularizer=tf.keras.regularizers.l1_l2(l1=1e-5, l2=1e-4))(hidden2)
 
         outputs = tf.keras.layers.Dense(
             self.num_actions, activation="tanh")(hidden)
@@ -40,19 +50,30 @@ class DDPG():
 
         # State as input
         state_input = tf.keras.layers.Input(shape=(self.num_states))
-        state_out_critic = tf.keras.layers.Dense(128, activation="relu", kernel_initializer= initializer)(state_input)
+        state_out_critic = tf.keras.layers.Dense(200, 
+        activation=tf.keras.layers.LeakyReLU(alpha=0.1),
+         kernel_initializer= initializer)(state_input)
 
         # Action as input
         action_input = tf.keras.layers.Input(shape=(self.num_actions))
-        action_out_critic = tf.keras.layers.Dense(200, activation="relu", kernel_initializer= initializer)(action_input)
+
+        action_out_critic = tf.keras.layers.Dense(128,
+        activation=tf.keras.layers.LeakyReLU(alpha=0.1),
+         kernel_initializer= initializer)(action_input)
 
         # Concatening 2 networks
         concat = tf.keras.layers.Concatenate()(
             [state_out_critic, action_out_critic]
             )
 
-        concat = tf.keras.layers.Dense(256, activation="relu", kernel_initializer= initializer)(concat)
-        out = tf.keras.layers.Dense(128, activation="relu", kernel_initializer= initializer)(concat)
+        concat = tf.keras.layers.Dense(256,
+         activation=tf.keras.layers.LeakyReLU(alpha=0.1), 
+        kernel_initializer= initializer)(concat)
+
+        out = tf.keras.layers.Dense(128, 
+        activation=tf.keras.layers.LeakyReLU(alpha=0.1),
+         kernel_initializer= initializer,
+         kernel_regularizer=tf.keras.regularizers.l1_l2(l1=1e-5, l2=1e-4))(concat)
 
         # Predicted Q(s,a)
         outputs = tf.keras.layers.Dense(1)(out)
@@ -116,7 +137,9 @@ def agent_out(agent, actor_model, state):
     s_nn = np.array(state, dtype=np.float32)
     s_nn = tf.expand_dims(s_nn, 0) #prepare state for NN
     a = actor_model(s_nn)
-    a = a + a*agents.std + agents.mu  # searching in action space
+    if np.random.rand(1) > 0.5:
+        a = a + np.random.normal(loc = config.MU, scale = agent.std)
+        agent.std = agent.std*(config.STD_DECAY**(-episode))
     a = a*agents.scale_effort #scale the action
     a0 = np.clip(a.numpy(), -agents.max_effort, agents.max_effort) #clip in max value
     return a0
@@ -132,7 +155,6 @@ def reward_callback(reward_msg):
 def done_callback(done_msg):
     global done
     done = done_msg.data
-
 
 if __name__ == '__main__':
     try:
@@ -168,11 +190,12 @@ if __name__ == '__main__':
         s0 = [0]*config.NUM_STATES
         a0 = np.array(0, dtype = np.float32)
         r = np.array(0, dtype = np.float32)
+        r_t = np.array(0, dtype = np.float32)
         start = Bool()
         action = Float32()
         done = False
         TS = config.TIME_STEP
-        i, j, episode, t,best_act = 0 , 0, 0, 0, 1e5
+        i, j, episode, t = 0 , 0, 0, 0
         states, actions, traj, total_trajectory, total_ac = [], [], [], [], []
 
         ''' Preparing for tensorboard'''
@@ -187,36 +210,42 @@ if __name__ == '__main__':
         pub = rospy.Publisher('agent_action', Float32, queue_size=10)
         start_simu = rospy.Publisher('start_simulation', Bool, queue_size=10)
 
+        if config.TRAIN == True:
+            print('Agent is in learning mode')
+        else:
+            print('Agent is in control mode')
+            
         while rospy.is_shutdown() == False or episode < config.MAX_EPISODE:
             #Starting simulation with simulink
             start.data = 1
             start_simu.publish(start)
             if j == 0:
-                print('[INFO] Asking simulation in simulink..')
+                print('[INFO] Starting simulation in simulink...')
                 j = j +1
             else:
                 start.data = 0
                 start_simu.publish(start)
 
             rospy.Subscriber('is_done', Bool, done_callback)
+            
             if done == False:
                 rospy.Subscriber("state", Float32MultiArray, state_callback)
                 rospy.Subscriber("reward", Float32, reward_callback)
                 a0 = agent_out(agents, actor_model, s0)
+                print('Action ', a0)
                 send_action(pub, action, a0)
-                print(a0)
-                r = r*agents.gamma**t + r
+                r_t = r*agents.gamma**t + r_t
+                print('Comulative reward : ', r_t)
                 if i < 2: 
                     #save states and actions at time t
                     states.append(s0)
                     actions.append(a0)
                     i = i +1
-                    if r > 300 or r < -300:
-                        r = -300
+
                     total_ac.append(a0)
                 else:
                     #fill trajectory with time t,t+1
-                    traj = [states[0], actions[0], states[1], actions[1], r]
+                    traj = [states[0], actions[0], states[1], actions[1], r_t]
                     total_trajectory.append(traj)
                     states = []
                     actions = []
@@ -228,44 +257,46 @@ if __name__ == '__main__':
                 done = False
                 loss_a, loss_c = [], []
                 num_sample = len(total_trajectory)
-                if not num_sample == 0:
-                    print('[INFO] Goal is reached or episode is finish. Stopping simulation...')
-                    print('[INFO] Learning for episode ', episode, ' ...')
-                    for i in range(len(total_trajectory[0])):
-                        aloss, closs = agents.update(total_trajectory[i][0],
-                        total_trajectory[i][1], 
-                        total_trajectory[i][4],
-                        total_trajectory[i][2], 
-                        total_trajectory[i][3] )
+                if config.TRAIN == True:
+                    if not num_sample == 0:
+                        print('[INFO] Episode is finish. Learning from episode ', episode, '  with ', num_sample, ' samples..')
+                        for i in range(num_sample):
+                            aloss, closs = agents.update(total_trajectory[i][0],
+                            total_trajectory[i][1], 
+                            total_trajectory[i][4],
+                            total_trajectory[i][2], 
+                            total_trajectory[i][3] )
 
-                        loss_a.append(aloss.numpy())
-                        loss_c.append(closs.numpy())
+                            loss_a.append(aloss.numpy())
+                            loss_c.append(closs.numpy())
+                        
+                        
                     
-                    avg_act = np.sum(np.array(loss_a))//num_sample
-                    avg_cri = np.sum(np.array(loss_c))//num_sample
-                    avg_rew = np.sum(np.array(total_trajectory)[:,4])//num_sample
+                        avg_act = np.sum(np.array(loss_a))//num_sample
+                        avg_cri = np.sum(np.array(loss_c))//num_sample
+                        avg_rew = np.sum(np.array(total_trajectory)[:,4])//num_sample
                     
-                    with train_summary_writer.as_default():
-                        tf.summary.scalar('Actor loss', np.sum(np.array(loss_a))//num_sample, step=episode)
-                        tf.summary.scalar('Critic loss', np.sum(np.array(loss_c))//num_sample, step=episode)
-                        tf.summary.scalar('Reward',  np.sum(np.array(total_trajectory)[:,4])//num_sample, step=episode)  
-                    
-                    print('---------- Learning on episode end ------------')
-                    j, i = 0, 0
-                    total_trajectory = []
-                    total_ac = []
-                    print('Saving agents weights...')
-                    actor_model.save_weights('./checkpoints/actor')
-                    critic_model.save_weights('./checkpoints/critic')
-                    episode = episode + 1    
+                        with train_summary_writer.as_default():
+                            tf.summary.scalar('Actor loss', np.sum(np.array(loss_a))//num_sample, step=episode)
+                            tf.summary.scalar('Critic loss', np.sum(np.array(loss_c))//num_sample, step=episode)
+                            tf.summary.scalar('Reward',  np.sum(np.array(total_trajectory)[:,4])//num_sample, step=episode)  
+                        episode = episode + 1
+                        j, i = 0, 0
+                        total_trajectory = []
+                        total_ac = []
+                        print('Learning phase is finish. Saving agents weights...')
+                        actor_model.save_weights('./checkpoints/actor')
+                        critic_model.save_weights('./checkpoints/critic')
+                        print('-------------------------')
                 s0 = [0]*config.NUM_STATES
                 a0 = np.array(0, dtype = np.float32)
                 r  = np.array(0, dtype = np.float32)
+
             if episode == config.MAX_EPISODE:
                 print('[INFO] All episod are finish. Saving entire models...')
-
-                actor_model.save('./model/actor')
-                critic_model.save('./model/critic')
+                if config.TRAIN == True:
+                    actor_model.save('./model/actor')
+                    critic_model.save('./model/critic')
                 break;
     except rospy.ROSInterruptException:
         pass
--- rl_connection/src/config.py
+++ rl_connection/src/config.py
@@ -3,20 +3,23 @@ import os
 if not os.path.exists('./checkpoints'):
     os.makedirs('./checkpoints/actor')
     os.makedirs('./checkpoints/critic')
+
 if not os.path.exists('./model'):
     os.makedirs('./model')
+
 #Agent configuration
 NUM_STATES = 4
 NUM_ACTIONS = 1
-SCALE_EFFORT = 25
-MAX_EFFORT = 24
-STD = 0.0 #0,1
-MU = 0.0 #-1,1
-CRITIC_LR = 1e-2
-ACTOR_LR = 1e-2
-PANDA = True
-BATCH = 16
+SCALE_EFFORT = 30
+MAX_EFFORT = 30
+STD = 0.2#0,1
+STD_DECAY = 1.3
+MU = 0 #-1,1
+CRITIC_LR = 2e-3
+ACTOR_LR = 1e-4
+PANDA = False
+TRAIN = True
 
 #Simulation configuration
-MAX_EPISODE = 15
-TIME_STEP = 1e-3
+MAX_EPISODE = 100
+TIME_STEP = 2e-3
