87cf2f278a8ffac428ed53f17f14525f56ec5a04
Giulio Mattera <97847032+giuliomattera@users.noreply.github.com>
2022-03-18 11:40:21 +0100

    Add files via upload

--- launch.bash
+++ launch.bash
@@ -1,5 +1,9 @@
 roscore &
+gnome-terminal -- cd rl_prj &
+rosrun rl_connection agent_node.py &
+gnome-terminal -- --logdir ./gradient_tape &
 gnome-terminal -- matlab -nodesktop -r "run('./simulink/matlab_node.m')" &
-gnome-terminal -- rosrun rl_connection agent_node.py &
+
+
 
 
