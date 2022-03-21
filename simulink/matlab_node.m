b0a458ffe05f5c68b8c480a5fc51857f818355e2
Giulio Mattera <97847032+giuliomattera@users.noreply.github.com>
2022-03-21 11:04:20 +0100

    Add files via upload

--- simulink/matlab_node.m
+++ simulink/matlab_node.m
@@ -1,10 +1,11 @@
 Episode_time = 15;
 rosinit()
+
 while true
     start = rossubscriber('start_simulation');
     ini = receive(start);
     ini = ini.Data;
-    if (ini == 1)
+    if (ini == true)
       disp('Starting simulation...')
       sim('simulink_nodes',Episode_time);
      else
